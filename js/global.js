a961fe4f5e08cc5ed1d7effd6ec643ffaffd4d20
kelsey vaught <kelsey.vaught92@gmail.com>
2017-02-21 13:49:11 -0500

    complete and working

--- js/global.js
+++ js/global.js
@@ -1,12 +1,13 @@
 // 1. Make a class called Trailer that extends Movie
-class _____ extends _____ {
+class Trailer extends Movie {
 
     // 2. Inside the class, add a constructor function that takes two arguments, a name and an id
-    constructor() {
-
+    constructor(movieTitleName, movieID) { 
         // 3. Call the parent constructor function using the super function
-
+         Super(movieTitleName, movieID);
         // 4. Assign a name and id instance property to the incoming name and id arguments
+        this.movieTitleName = 'title'
+        this.movieID = 'youtube ID'
 
         // 5. This method will show the button on the screen, no changes necessary
         this.renderButton()
@@ -14,3 +15,22 @@ class _____ extends _____ {
 }
 
 // 6. Create four instances of the Trailer class, passing in the name of a film as the first argument and its corresponding YouTube ID as the second argument
+
+var hamletTrailer = movieOne (moveTitleName, movieID);
+    hamletTrailer.movieTitleName = 'Hamlet 2';
+    hamletTrailer.movieID = 'FCQr7x9WtJ0';
+
+var jackieTrailer = movieTwo (movieTitleName, movieID);
+    jackieTrailer.movieTitleName = 'Jackie';
+    jackieTrailer.movieID = 'g9pW3B8Ycc4';
+
+var masterTrailer = movieThree (movieTitleName, movieID);
+    masterTrailer.moveTitleName = 'The Master';
+    masterTrailer.movieID = 'DaPYuUpNEAo';
+
+var parisTrailer = movieFour (movieTitleName, movieID);
+    parisTrailer.moveTitleName = 'Paris is Burning';
+    parisTrailer.movieID = '78TAbjx43rk';    
+
+    
+
